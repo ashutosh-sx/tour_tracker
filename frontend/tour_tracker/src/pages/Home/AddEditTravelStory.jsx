eec5bf3244d63a46a93aa8aa9a3cddc4bd7bac4e
Ashutosh Saxena <ashu2002saxena@gmail.com>
2025-01-15 06:10:36 +0530

    New story functionality added

--- frontend/tour_tracker/src/pages/Home/AddEditTravelStory.jsx
+++ frontend/tour_tracker/src/pages/Home/AddEditTravelStory.jsx
@@ -1,13 +1,119 @@
-import React from 'react'
+import React, { useState } from "react";
+import { MdAdd, MdDeleteOutline, MdUpdate, MdClose } from "react-icons/md";
+import DateSelector from "../../components/Input/DateSelector";
+import ImageSelector from "../../components/Input/ImageSelector";
+import TagInput from "../../components/Input/TagInput";
 
-const AddEditTravelStory = () => {
+const AddEditTravelStory = ({
+  type,
+  storyInfo,
+  onClose,
+  getAllTravelStories,
+}) => {
+
+  const [title, setTitle] = useState("");
+  const [storyImg, setStoryImg] = useState(null);
+  const [story, setStory] = useState("");
+  const [visitedLocation, setVisitedLocation] = useState([]);
+  const [visitedDate, setVisitedDate] = useState(null);
+  const [error, setError] = useState("");
+
+   //Add New travel Story
+   const addNewTravelStory = async () => {}
+
+  //Update travel Story
+  const updateTravelStory = async () => {}
+
+  const handleAddOrUpdateClick = () => {
+    console.log("Input Data:", { title, storyImg, story, visitedLocation, visitedDate });
+  
+    if (!title) {
+      setError("Please enter the title");
+      return;
+    }
+  
+    if (!story) {
+      setError("Please enter the story");
+      return;
+    }
+  
+    setError("");
+  
+    if (type === "edit") {
+      updateTravelStory();
+    } else {
+      addNewTravelStory();
+    }
+  };
+
+  const handleDeleteStoryImg = () => {
+    // Implement the logic for deleting the story
+    // After deletion, you might want to call getAllTravelStories() and onClose()
+  };
+
   return (
     <div>
-      Add Travel Story
+      <div className="flex items-center justify-between">
+        <h5 className="text-xl font-medium text-slate-700">
+          {type === "add" ? "Add Story" : "Update Story"}
+        </h5>
+        <div>
+          <div className="flex items-center gap-3 bg-cyan-50/50 p-2 rounded-lg">
+            {type === "add" ? (
+              <button className="btn-small" onClick={handleAddOrUpdateClick}>
+                <MdAdd className="text-lg" /> ADD STORY
+              </button>
+            ) : (
+              <>
+                <button className="btn-small" onClick={handleAddOrUpdateClick}>
+                  <MdUpdate className="text-lg" /> UPDATE STORY
+                </button>
+                <button className="btn-small btn-delete" onClick={handleDeleteClick}>
+                  <MdDeleteOutline className="text-lg" /> DELETE
+                </button>
+              </>
+            )}
+            <button className="" onClick={onClose}>
+              <MdClose className="text-xl text-slate-400" />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="flex-1 flex flex-col gap-2 pt-4">
+          <label className="input-label">TITLE</label>
+          <input
+            type="text"
+            className="text-2xl text-slate-950 outline-none"
+            placeholder="A Day at the Taj"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <div className="my-3">
+            <DateSelector date={visitedDate} setDate={setVisitedDate} />
+          </div>
+
+          <ImageSelector image ={storyImg} setImage = {setStoryImg} handleDeleteImg={handleDeleteStoryImg}/>
+          <div className="flex flex-col gap-2 mt-4">
+            <label className="input-label">STORY</label>
+            <textarea
+              type="text"
+              className="text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded"
+              placeholder="Your Story"
+              rows={10}
+              value={story}
+              onChange={(e) => setStory(e.target.value)}
+            />
+          </div>
+          <div className="pt-3">
+            <label className="input-label">VISITED LOCATIONS</label>
+            <TagInput tags={visitedLocation} setTags={setVisitedLocation} />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default AddEditTravelStory;
 
-//2:36:17
